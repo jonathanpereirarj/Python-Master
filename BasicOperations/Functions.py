5ec99089952e2fad3c29b47f2412bb2e3ab74341
Jonathan Pereira <jonathan.pereira@ceptis.com.br>
2019-07-26 18:56:20 -0300

    Synthax test

---
+++ BasicOperations/Functions.py
@@ -0,0 +1,16 @@
+'''
+Created on 26 de jul de 2019
+
+@author: JPereira3
+'''
+
+def test (number):
+    number = 42
+    return number
+    
+def car_rental_cost (days):
+    if (days > 7):
+        return (days*40) - 50
+    elif (days > 3):
+        return (days*40) - 20
+
